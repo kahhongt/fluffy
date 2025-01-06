f82ee873dde2e31196e2165a8f0e6e51b20558f1
Kah Hong <kahhong.tai@gmail.com>
2025-01-06 15:00:38 +0800

    minor edit to charis description

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,7 +20,7 @@ function Home() {
                     We are an applied AI research and development company.
                 </h1>
                 <p className='text-lg text-secondary dark:text-dark-secondary max-w-4xl text-center px-4 mb-8'>
-                        We are currently building 2 products: <strong> Solaris </strong> - a multi-platform decision-making agent that operates across various messaging applications globally, and <strong> Charis </strong> - a patient monitoring system that uses AI to help improve mental health outcomes for psychology and counselling clinics.
+                        We are currently building 2 products: <strong> Solaris </strong> - a multi-platform decision-making agent that operates across various messaging applications globally, and <strong> Charis </strong> - a patient monitoring system that uses AI to improve mental health outcomes for psychology and counselling clinics.
                 </p>
                 <a href='https://t.me/diametricbot' target='_blank'>
                     <button className='bg-accent text-primary dark:bg-dark-primary dark:text-dark-background rounded-lg px-6 py-3 flex items-center gap-3 text-lg'>
